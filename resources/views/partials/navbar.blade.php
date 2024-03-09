48632d1f6894d0ca78a75802fc2e030315b784fb
Sigimatika <sigitechindo@gmail.com>
2024-03-09 11:02:20 +0700

    ini baru lagi

--- resources/views/partials/navbar.blade.php
+++ resources/views/partials/navbar.blade.php
@@ -18,7 +18,7 @@
 
         <ul class="navbar-list">
 
-            <li class="navbar-item">
+            {{-- <li class="navbar-item">
             <a href="/" class="navbar-link <?= ($active === "beranda") ? 'active' : ''; ?>" data-navbar-link>Beranda</a>
             </li>
         
@@ -91,7 +91,81 @@
             <a href="/login" class="btn-2 <?= ($active === "login") ? 'active' : ''; ?> text-dark"><i class="bi bi-box-arrow-in-right mr-2 text-dark"></i> Login</a>
         </li>
 
+        @endauth --}}
+
+        <!-- Navbar untuk desktop -->
+<ul class="navbar-list desktop-navbar">
+    <!-- Item navbar -->
+    <li class="navbar-item">
+        <a href="/" class="navbar-link <?= ($active === "beranda") ? 'active' : ''; ?>" data-navbar-link>Beranda</a>
+    </li>
+
+    <li class="navbar-item">
+        <a href="/tentang" class="navbar-link <?= ($active === "tentang") ? 'active' : ''; ?>" data-navbar-link>Tentang</a>
+    </li>
+
+    <li class="navbar-item">
+        <a href="/layanan" class="navbar-link <?= ($active === "layanan") ? 'active' : ''; ?>" data-navbar-link>Layanan</a>
+    </li>
+
+    <li class="navbar-item">
+        <a href="/donasi" class="navbar-link <?= ($active === "donasi") ? 'active' : ''; ?>" data-navbar-link>Donasi</a>
+    </li>
+
+    <li class="navbar-item">
+        <a href="/" class="navbar-link <?= ($active === "project") ? 'active' : ''; ?>" data-navbar-link>Project</a>
+        <ul class="dropdown">
+            @foreach ($categories as $cat)
+            <li>
+                <a class="navbar-link <?= ($active === $cat->slug) ? 'active' : ''; ?>" data-navbar-link href="/blogproject?category={{ $cat->slug }}">
+                    <i class="{{ $cat->nama_kategori }}"></i> {{ $cat->nama_kategori }}
+                </a>
+            </li>
+            @endforeach
+        </ul>
+    </li>
+
+    <li class="navbar-item">
+        <a href="#footer" class="navbar-link <?= ($active === "kontak") ? 'active' : ''; ?>" data-navbar-link>Kontak</a>
+    </li>
+</ul>
+
+<!-- Navbar untuk mobile (hamburger icon) -->
+<div class="mobile-navbar">
+    <!-- Hamburger icon -->
+    <div class="hamburger-icon" onclick="toggleNavbar()">
+        <!-- Icon bars -->
+        <div class="bar">hai</div>
+        <div class="bar">hai</div>
+        <div class="bar">hai</div>
+    </div>
+
+    <!-- Navbar links (sembunyi pada awalnya) -->
+    <ul class="navbar-list" id="mobileNavbar">
+        <!-- Item navbar -->
+        <!-- ... -->
+        @auth
+        <li class="nav-item dropdown">
+            <a class="nav-link dropdown-toggle text-dark" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                Welcome, {{ auth()->user()->name }}
+            </a>
+            <a class="btn-2" href="/dashboard">
+                <ion-icon name="list-circle-outline">
+                </ion-icon>
+            </a>
+            <form action="/logout" method="post">
+                @csrf
+                <button type="submit" class="btn-2"><ion-icon name="log-out-outline"></ion-icon></button>
+            </form>
+        </li>
+        @else
+        <li class="nav-item">
+            <a href="/login" class="btn-2 <?= ($active === "login") ? 'active' : ''; ?> text-dark"><i class="bi bi-box-arrow-in-right mr-2 text-dark"></i> Login</a>
+        </li>
         @endauth
+    </ul>
+</div>
+
 
         </ul>
 
