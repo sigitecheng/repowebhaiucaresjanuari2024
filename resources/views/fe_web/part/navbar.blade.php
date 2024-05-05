a1bce198eb90bb211812ff8e6a2e6e63490fe848
Sigimatika <sigitechindo@gmail.com>
2024-05-06 00:53:28 +0700

    ini baruff

--- resources/views/fe_web/part/navbar.blade.php
+++ resources/views/fe_web/part/navbar.blade.php
@@ -6,14 +6,19 @@
                     <div class="icon p-1 me-2 text-custom">
                         <img class="img-fluid" src="/frontend/img/logohaiucare.png" alt="Icon" style="width: 48px; height: 48px;">
                     </div>
-                    <h2 class="text-custom" style="font-family: 'Quicksand', sans-serif;"><span class="text">HaiuCare Indonesia</span></h2>
+                    <h2 class="text-custom" style="font-family: 'Quicksand', sans-serif; font-size:24px"><span class="text">HaiuCare Indonesia</span></h2>
 
                     {{-- <h2 class="m-0 text-custom" >HaiuCare Indonesia</h2> --}}
                 </a>
-                
-                {{-- <button type="button" class="navbar-toggler" data-bs-toggle="collapse" data-bs-target="#navbarCollapse">
+{{--                 
+                <button type="button" class="navbar-toggler" data-bs-toggle="collapse" data-bs-target="#navbarCollapse">
                     <span class="navbar-toggler-icon"></span>
                 </button> --}}
+
+                <button type="button" class="navbar-toggler" id="navbar-toggler" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+
 
                 <div class="collapse navbar-collapse" id="navbarCollapse">
                     <div class="navbar-nav ms-auto text-custom" style="font-family: 'Quicksand', sans-serif;">
@@ -44,6 +49,18 @@
                     <a href="/login" class="button-custom px-3 d-none d-lg-flex text-white" style="font-family: 'Quicksand', sans-serif;"><i class="fas fa-sign-in-alt mr-2 mt-1"></i>LOGIN</a>
                 </div>
             </nav>
+
+            
+                <script>
+                    document.getElementById('navbar-toggler').addEventListener('click', function() {
+                        var navbarCollapse = document.getElementById('navbarCollapse');
+                        if (navbarCollapse.classList.contains('show')) {
+                            navbarCollapse.classList.remove('show');
+                        } else {
+                            navbarCollapse.classList.add('show');
+                        }
+                    });
+                </script>
         </div>
         <!-- Navbar End -->
 
